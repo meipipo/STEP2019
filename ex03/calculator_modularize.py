828fc89915bfb8a6fabb18bdcba7681a8c22ab00
meipipo <meteor1038@gmail.com>
2019-06-09 13:27:17 +0900

    parentheses

--- ex03/calculator_modularize.py
+++ ex03/calculator_modularize.py
@@ -34,6 +34,16 @@ def readDiv(line, index):
   return token, index + 1
 
 
+def readLPar(line, index):
+  token = {'type': 'LPAR'}
+  return token, index + 1
+
+
+def readRPar(line, index):
+  token = {'type': 'RPAR'}
+  return token, index + 1
+
+
 def tokenize(line):
   tokens = []
   index = 0
@@ -48,6 +58,10 @@ def tokenize(line):
       (token, index) = readMult(line, index)
     elif line[index] == '/':
       (token, index) = readDiv(line, index)
+    elif line[index] == '(':
+      (token, index) = readLPar(line, index)
+    elif line[index] == ')':
+      (token, index) = readRPar(line, index)
     else:
       print('Invalid character found: ' + line[index])
       exit(1)
@@ -55,6 +69,43 @@ def tokenize(line):
   return tokens
 
 
+# handle parentheses (returns updated tokens)
+def evaluate_Par(tokens):
+  index = 0
+  rpar_index = len(tokens)
+  have_lpar = []
+  have_rpar = []
+  # indexes of '(' and ')' in tokens
+  for i in range(len(tokens)):
+    if tokens[i]['type'] == 'LPAR':
+      have_lpar.append(i)
+    elif tokens[i]['type'] == 'RPAR':
+      have_rpar.append(i)
+  if len(have_lpar) != len(have_rpar): # if the number of '(' and ')' are unequal
+    print('Invalid syntax 1')
+    exit(1)
+  popcount = 0 # to renew indexes after updating tokens
+  while have_lpar and have_rpar:
+      lpar_index = have_lpar.pop()
+      rpar_index = [x for x in have_rpar if x > lpar_index]
+      rpar_index = rpar_index[0]
+      have_rpar.remove(rpar_index)
+      rpar_index -=  popcount # renew index
+      popcount = 0
+      # evaluate tokens inside parentheses and update tokens
+      inpar_tokens = tokens[lpar_index+1:rpar_index]
+      tokens[lpar_index] = {'type': 'NUMBER', 'number': evaluate_normal(inpar_tokens)}
+      del tokens[lpar_index+1:rpar_index+1]
+      # check if index should be updated
+      if have_lpar:
+          next_lpar = have_lpar[len(have_lpar)-1]
+          next_rpar = [x for x in have_rpar if x > next_lpar]
+          if rpar_index < next_rpar[0]:
+              popcount = rpar_index - lpar_index
+  return tokens
+
+
+# multiplication and division (returns updated tokens)
 def evaluate_MD(tokens):
   index = 1
   while index < len(tokens):
@@ -76,12 +127,13 @@ def evaluate_MD(tokens):
       elif tokens[index - 1]['type'] == 'PLUS' or tokens[index - 1]['type'] == 'MINUS':
         index += 1
       else:
-        print('Invalid syntax')
+        print('Invalid syntax 3')
         exit(1)
     index += 1
   return tokens
 
 
+# plus and minus
 def evaluate_PM(tokens):
   answer = 0
   tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
@@ -93,16 +145,21 @@ def evaluate_PM(tokens):
       elif tokens[index - 1]['type'] == 'MINUS':
         answer -= tokens[index]['number']
       else:
-        print('Invalid syntax')
+        print('Invalid syntax 4')
         exit(1)
     index += 1
   return answer
 
 
+# normal + - * / calculator
+def evaluate_normal(tokens):
+  return evaluate_PM(evaluate_MD(tokens))
+
+
 def test(line):
   tokens = tokenize(line)
-  tokens = evaluate_MD(tokens)
-  actualAnswer = evaluate_PM(tokens)
+  tokens = evaluate_Par(tokens)
+  actualAnswer = evaluate_normal(tokens)
   expectedAnswer = eval(line)
   if abs(actualAnswer - expectedAnswer) < 1e-8:
     print("PASS! (%s = %f)" % (line, expectedAnswer))
@@ -123,6 +180,14 @@ def runTest():
   test("5-1.5*10/3")
   test("2-1.5*10-6/30")
   test("1.4/0.7")
+  test("(1+5)")
+  test("3*(1+5)")
+  test("3*(1+5/4)")
+  test("100*((7-5*4-3/92)/6)+7/5")
+  test("(5+4*2)/3-2*(4+5)")
+  test("(1+5)/3-4*(6*(12/7+8))+15")
+  test("((1+2)*3)/4-7*2+(4+3)*30")
+  test("1+5/675/34*(2+(3/4)+(5-7*34)-2)")
   # test("3-9/0")
   # test("2/0")
   # test("4^5")
@@ -134,6 +199,6 @@ while True:
   print('> ', end="")
   line = input().replace(" ", "")
   tokens = tokenize(line)
-  tokens = evaluate_MD(tokens)
-  answer = evaluate_PM(tokens)
+  tokens = evaluate_Par(tokens)
+  answer = evaluate_normal(tokens)
   print("answer = %f\n" % answer)
